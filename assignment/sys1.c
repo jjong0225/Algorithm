9ceddfdd9d2c1e690b6055692322269b409515b3
jjong0225 <jjong0225@naver.com>
2021-03-25 21:36:59 +0900

    3/25

--- assignment/sys1.c
+++ assignment/sys1.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 #define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c\n"
 #define BYTE_TO_BINARY(byte)  \
   (byte & 0x80 ? '1' : '0'), \
@@ -19,9 +21,9 @@ unsigned char btol_1byte(unsigned *input_byte)
     return return_d;
 }
 
-int btol_4byte(unsigned *input_byte)
+unsigned btol_4byte(unsigned *input_byte)
 {
-    int return_d = 0;
+    unsigned return_d = 0;
     for(int i=0; i<4; i++)
     {
         return_d = return_d | input_byte[i];
@@ -31,25 +33,96 @@ int btol_4byte(unsigned *input_byte)
     return return_d;
 }
 
-void read_integer_data(unsigned *input_byte, int n)
+unsigned long long int btol_8byte(unsigned *input_byte)
 {
-    printf("------Integer------\n");
-    for(int i=0; i<n/4; i++)
+    unsigned long long int return_d = 0;
+    for(int i=0; i<8; i++)
     {
-        int ouptut_int = btol_4byte(&input_byte[i*4]);
-        printf("%d\n", ouptut_int);
+        return_d = return_d | input_byte[i];
+        if(i != 7)
+            return_d = return_d << 8;
     }
-    printf("------Integer------\n");
+    return return_d;
 }
 
-void read_char_data(unsigned *input_byte, int n)
+void read_signed_char_data(unsigned *input_byte, int n)
 {
-    printf("------Char------\n");
+    printf("------Signed_Char------\n");
     for(int i=0; i<n; i++)
     {
-        printf("%d\n", btol_1byte(input_byte + i));
+        char tmp_c = 0;
+        tmp_c = tmp_c | btol_1byte(input_byte + i);
+        printf("%d\t", tmp_c);
     }
-    printf("------Char------\n");    
+}
+
+void read_ASCII_codes_data(unsigned *input_byte, int n)
+{
+    printf("------ASCII_codes------\n");
+    for(int i=0; i<n; i++)
+    {
+        char tmp_c = 0;
+        tmp_c = tmp_c | btol_1byte(input_byte + i);
+        if(tmp_c >32 && tmp_c < 127)
+            printf("%c\t", tmp_c);
+        else
+            printf(".\t", tmp_c);
+    }
+}
+
+void read_unsigned_char_data(unsigned *input_byte, int n)
+{
+    printf("------Unsigned_Char------\n");
+    for(int i=0; i<n; i++)
+    {
+        unsigned char tmp_c = btol_1byte(input_byte + i);
+        printf("%d\t", tmp_c);
+    }
+}
+
+void read_signed_int_data(unsigned *input_byte, int n)
+{
+    printf("------Signed_int------\n");
+    for(int i=0; i<n/4; i++)
+    {
+        int ouptut_int = 0;
+        ouptut_int = ouptut_int | btol_4byte(&input_byte[i*4]);
+        printf("%d\t", ouptut_int);
+    }
+}
+
+void read_unsigned_int_data(unsigned *input_byte, int n)
+{
+    printf("------Unsigned_int------\n");
+    for(int i=0; i<n/4; i++)
+    {
+        unsigned int ouptut_int = btol_4byte(&input_byte[i*4]);
+        printf("%u\t", ouptut_int);
+    }
+}
+
+void read_signed_float_data(unsigned *input_byte, int n)
+{
+    printf("------Signed_float------\n");
+    for(int i=0; i<n/4; i++)
+    {
+        unsigned int ouptut_int = btol_4byte(&input_byte[i*4]);
+        float tmp_float = 0;
+        memcpy(&tmp_float, &ouptut_int, 4);
+        printf("%f\t", tmp_float);
+    }
+}
+
+void read_signed_double_data(unsigned *input_byte, int n)
+{
+    printf("------Signed_double------\n");
+    for(int i=0; i<n/8; i++)
+    {
+        unsigned long long int ouptut_lld = btol_8byte(&input_byte[i*8]);
+        double output_lf = 0;
+        memcpy(&output_lf, &ouptut_lld, 8);
+        printf("%lf\t", output_lf);
+    }
 }
 
 int main ()
@@ -86,7 +159,18 @@ int main ()
         input_byte[total_size++] = insert_c;
     }
     total_size--;
-    read_integer_data(input_byte,total_size);
+    read_signed_char_data(input_byte,total_size);
     printf("\n");
-    read_char_data(input_byte,total_size);
+    read_ASCII_codes_data(input_byte,total_size);
+    printf("\n");
+    read_unsigned_char_data(input_byte,total_size);
+    printf("\n");
+    read_signed_int_data(input_byte,total_size);
+    printf("\n");
+    read_unsigned_int_data(input_byte,total_size);
+    printf("\n");
+    read_signed_float_data(input_byte,total_size);
+    printf("\n");
+    read_signed_double_data(input_byte,total_size);
+    printf("\n");
 }
