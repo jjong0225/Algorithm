58ef1c914f5a26f69a26035108dd5d17d3dc991f
jjong0225 <jjong0225@naver.com>
2021-04-10 06:42:57 -0700

    4/10

--- assignment/sys/editro/test.c
+++ assignment/sys/editro/test.c
@@ -2,7 +2,11 @@
 
 int main ()
 {
-    char *readonly_str = "hello software";
+    char *readonly_str = "softwaresoftware\n";
+    char *readonly_str_2 = "software\n";
+    char *readonly_str_3 = "hello the school of software\n";
     printf("%s", readonly_str);
+    printf("%s", readonly_str_2);
+    printf("%s", readonly_str_3);
     return 0;
 }
