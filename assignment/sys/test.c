8c6dcdc6ef5bdf9b6487139d3f259ca84f5ea8cd
jjong0225 <jjong0225@naver.com>
2021-04-16 11:18:01 -0700

    4/17

--- assignment/sys/test.c
+++ assignment/sys/test.c
@@ -1,17 +1,101 @@
 #include<stdio.h>
 #include<unistd.h>
+#include<sys/wait.h>
+#include <fcntl.h>
 
-int main()
+#define NUM_OF_PROCESS 5
+
+int main(int argc, char *argv[])
 {
-    int counter = 0;
-    for(int i=1; i<=6; i++)
+    if(argc < 2)
     {
-        int pid = fork();
-        if(pid == 0)
+        printf("Add file path following executive file path\n");
+        return 0;
+    }
+    int readfile_fd = open(argv[1], O_RDONLY|O_SYNC);
+
+    
+
+    int p_counter = 0;
+    int fd_arr[NUM_OF_PROCESS][2] = {0};
+    for(int i=0; i<NUM_OF_PROCESS; i++)
+    {
+        if(pipe(fd_arr[i]) == -1)
         {
-            counter = i;
+            printf("something went wrong");
+            return 0;
+        }
+    }
+
+    for(int i=1; i<NUM_OF_PROCESS; i++)
+    {
+        int p_count = fork();
+        if(p_count == 0)
+        {
+            p_counter = i;
             break;
         }
     }
-    printf("%d : %d번째 프로세스", getpid(), counter);
+    int send_pipe_fd = (p_counter) % NUM_OF_PROCESS;
+    int recv_pipe_fd = (p_counter + NUM_OF_PROCESS - 1) % NUM_OF_PROCESS;
+
+    for(int i=0; i<NUM_OF_PROCESS; i++)
+    {
+        if(i != send_pipe_fd && i!= recv_pipe_fd)
+            close(fd_arr[i][0]), close(fd_arr[i][1]);
+    }
+
+    while(1)
+    {
+        char tmp_buff[1] = {0};
+        read(fd_arr[recv_pipe_fd][0], tmp_buff, 1);
+        int next_sig = 0;
+
+        if(tmp_buff[0] == -1)
+        {
+            if(p_counter == NUM_OF_PROCESS -1)
+            {
+                char tmp_buff_4[1] = {-2};
+                write(fd_arr[recv_pipe_fd][1], tmp_buff_4, 1);        
+                break;
+            }
+            else
+            {
+                char tmp_buff_4[1] = {-1};
+                write(fd_arr[send_pipe_fd][1], tmp_buff_4, 1);        
+
+                char tmp_buff[1] = {0};
+                read(fd_arr[send_pipe_fd][0], tmp_buff, 1);
+                if(tmp_buff[0] == -2)
+                    break;
+            }
+        }
+        else
+        {
+            char tmp_buff_2[1] = {0};
+            read(readfile_fd, tmp_buff_2, 1);
+
+            if(tmp_buff_2[0] == EOF)
+                next_sig = -1;
+            else
+            {
+                printf("%d[%d번째 프로세스] : %c\n", getpid(), p_counter, tmp_buff_2[0]);
+                next_sig = 1;
+            }
+            char tmp_buff_3[1] = {0};
+            tmp_buff_3[0] = next_sig;
+            write(fd_arr[send_pipe_fd][1], tmp_buff_3, 1);
+        }
+    }
+
+    char tmp_buff_3[1] = {0};
+    tmp_buff_3[0] = -2;
+    write(fd_arr[recv_pipe_fd][1], tmp_buff_3, 1);
+
+
+
+    int status;
+    wait(&status);
+
+    return 0;
 }
